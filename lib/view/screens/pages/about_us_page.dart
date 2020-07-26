aba2ac7e4a4d0f38c9f7a498b96f319f8d300d6f
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-07-26 19:19:28 +0900

    Implement the AboutUsPage

--- lib/view/screens/pages/about_us_page.dart
+++ lib/view/screens/pages/about_us_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class AboutAsPage extends StatefulWidget {
   @override
@@ -6,15 +7,37 @@ class AboutAsPage extends StatefulWidget {
 }
 
 class _AboutAsPageState extends State<AboutAsPage> {
+  final String animatedText = 'News Feed App';
+  bool _selected = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        // TODO:
-        body: Container(
-          child: Center(
-            child: Text('AboutUsPage'),
+        body: Center(
+          child: AnimatedContainer(
+            width: _selected ? 300.0 : 50.0,
+            height: _selected ? 200.0 : 25.0,
+            alignment: _selected ? Alignment.center : Alignment.topCenter,
+            duration: Duration(milliseconds: 1000),
+            curve: Curves.fastOutSlowIn,
+            child: AutoSizeText(
+              animatedText,
+              style: TextStyle(fontSize: 40.0),
+              minFontSize: 6.0,
+              maxLines: 1,
+              overflow: TextOverflow.visible,
+            ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          tooltip: 'start animation',
+          onPressed: () {
+            setState(() {
+              _selected = !_selected;
+            });
+          },
         ),
       ),
     );
